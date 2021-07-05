cc6b338307b432d76d5d0477be2d6cb0b69e6dea
manismk <manismkmech@gmail.com>
2021-07-05 22:54:21 +0530

    upto game

---
+++ emoji_guesser/index.js
@@ -0,0 +1,80 @@
+const cards = document.querySelectorAll(".card");
+console.log(cards);
+
+//variables
+var isFlipped = false;
+var firstCard;
+var secondCard;
+
+cards.forEach((card) => card.addEventListener("click", flip));
+
+function flip() {
+  //   console.log("Card flipped");
+  // console.log(this);
+  this.classList.add("flip");
+  if (!isFlipped) {
+    isFlipped = true;
+    firstCard = this;
+  } else {
+    secondCard = this;
+    console.log(firstCard);
+    console.log(secondCard);
+
+    checkIt();
+  }
+}
+
+function checkIt() {
+  //   console.log("Checking...");
+  if (
+    firstCard.dataset.image === secondCard.dataset.image &&
+    firstCard.style.order != secondCard.style.order
+  ) {
+    success();
+  } else {
+    fail();
+  }
+}
+
+function success() {
+  //   console.log("Success");
+  firstCard.removeEventListener("click", flip);
+  secondCard.removeEventListener("click", flip);
+  reset();
+  alert("found it");
+}
+
+function fail() {
+  // cards.forEach((card) =>
+  //   card.addEventListener("click", function (e) {
+  //     e.stopPropagation();
+  //   })
+  // );
+  //   console.log("Failed");
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    // cards.forEach((card) =>
+    //   card.removeEventListener("click", function (e) {
+    //     e.stopPropagation();
+    //   })
+    // );
+
+    reset();
+  }, 800);
+}
+
+function reset() {
+  isFlipped = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+//TODO: shuffle
+
+(function shuffle() {
+  cards.forEach((card) => {
+    var index = Math.floor(Math.random() * 16);
+    card.style.order = index;
+  });
+})();
